f9e9a144ab9d9ffcadfed7cb12714979e19b68cb
Kavian Darvish <kavian1991@gmail.com>
2020-02-28 13:24:28 -0500

    Updates

---
+++ precache-manifest.08d361386a31c47dbc24de7e663e9274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a6bcd57a7d3913e4ac83abdf211eb1",
+    "url": "/employee-dashboard/index.html"
+  },
+  {
+    "revision": "5f5290207a2e4974e71b",
+    "url": "/employee-dashboard/static/css/main.e2fc6af4.chunk.css"
+  },
+  {
+    "revision": "2ec91ae2a729675419eb",
+    "url": "/employee-dashboard/static/js/2.24859a87.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/employee-dashboard/static/js/2.24859a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5290207a2e4974e71b",
+    "url": "/employee-dashboard/static/js/main.1abf3290.chunk.js"
+  },
+  {
+    "revision": "3624d42a413b411258ac",
+    "url": "/employee-dashboard/static/js/runtime-main.de203d6a.js"
+  }
+]);
